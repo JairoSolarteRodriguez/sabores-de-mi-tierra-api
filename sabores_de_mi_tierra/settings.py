2a3ef0d7d95c5b072a1335670a8eb7d6f65fd33d
Valentin <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2022-08-06 12:08:19 -0500

    Update settings.py

--- sabores_de_mi_tierra/settings.py
+++ sabores_de_mi_tierra/settings.py
@@ -14,10 +14,7 @@ from pathlib import Path
 import os
 
 import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
 # reading .env file
 environ.Env.read_env()
@@ -30,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1231212dasdasd'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # De string a booleano
-DEBUG = True
+DEBUG = env('DEBUG')
 
 
 ALLOWED_HOSTS = ['*']
