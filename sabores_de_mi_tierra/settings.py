52d14c507b89032df017afa30f690472a883c91a
Valentin <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2022-08-06 12:09:46 -0500

    Update settings.py

--- sabores_de_mi_tierra/settings.py
+++ sabores_de_mi_tierra/settings.py
@@ -16,9 +16,6 @@ import os
 import environ
 env = environ.Env()
 
-# reading .env file
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
