1ddc28f9a1098ca6c9e397027cae9d8dc19d3c8d
Valentin <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2022-08-06 12:06:49 -0500

    Update settings.py

--- sabores_de_mi_tierra/settings.py
+++ sabores_de_mi_tierra/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# reading .env file
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
