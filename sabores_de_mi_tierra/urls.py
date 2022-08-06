8f0db236d458fcdd9994bbf9fb3345cdfdc7bc9e
Valentin <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2022-08-06 13:28:48 -0500

    Update urls.py

--- sabores_de_mi_tierra/urls.py
+++ sabores_de_mi_tierra/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-import environ
-
-env = environ.Env()
-
-# reading .env file
-environ.Env.read_env()
-API_KEY = env('API_KEY')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(f'users/{API_KEY}/', include('users_app.urls')),
+    path(f'users/YDJdT640N8Hspnm44867pg0Pcsar3FBQ/', include('users_app.urls')),
     path('api/', include('api.urls')),
 ]
